33958277fbc90e924e41dd0ac45d5fdf09c733a7
Adam Breaux <breauxaj@gmail.com>
2014-07-08 09:31:16 -0400

    not necessary

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,14 +6,9 @@ define collectd::service (
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'collectd',
   }
 
-  $depends = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => 'collectd',
-  }
-
   service { $service:
     ensure  => $ensure,
     enable  => $enable,
-    require => Package[$depends],
   }
 
 }
