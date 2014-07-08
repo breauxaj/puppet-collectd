525602fdf12ebcf527e54a34a1af4b14930aa68d
Adam Breaux <breauxaj@gmail.com>
2014-07-08 09:34:29 -0400

    added docs

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,31 @@
+# Define: collectd::service
+#
+# This define manages the Collectd service
+#
+# Parameters:
+#
+#  ensure:
+#    Controls the state of the service
+#
+#  enable:
+#    Controls the service start on boot
+#
+# Actions:
+#   - Stops/starts the Collectd service
+#
+# Requires:
+#
+#  EPEL repository
+#
+# Sample Usage:
+#
+#  To enable the service, use:
+#
+#    collectd::service { 'default':
+#      ensure => running,
+#      enable => true
+#    }
+#
 define collectd::service (
   $ensure,
   $enable
